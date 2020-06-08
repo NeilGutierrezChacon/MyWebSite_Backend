8a94db8fb12f31d5562505c860ccaa76ddacbc04
NeilGutierrezChacon <n.g.ch43@gmail.com>
2020-06-08 23:12:00 +0200

    Rutas de la pagina añadidas y correcion de la estructura del Home

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,16 @@ router.get("/", (req, res) => {
   res.render("index.html");
 });
 
+router.get("/MyProjects", (req, res) => {
+  res.render("my_projects.html");
+});
+
+router.get("/Blog", (req, res) => {
+  res.render("blog.html");
+});
+
+router.get("/Contact", (req, res) => {
+  res.render("contact.html");
+});
+
 module.exports = router;
